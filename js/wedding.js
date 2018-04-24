5f4178675c1187739ab39d98ad34726e63e97a2d
RobinN7 <robin.beilvert@gmail.com>
2018-04-24 21:33:02 +0200

    replaced ggle maps w/ openstreetmaps

--- js/wedding.js
+++ js/wedding.js
@@ -1,4 +1,22 @@
+var map = L.map('map').setView([47.256825, -2.223079], 12);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var markergare				= L.marker([47.286429, -2.210980]).addTo(map);
+var markerbusgare			= L.marker([47.285610, -2.211772]).addTo(map);
+var markerbusmairie 		= L.marker([47.282663, -2.261125]).addTo(map);
+var markermairieimmaculee	= L.marker([47.282762, -2.260120]).addTo(map);
+var markercasinostbrevin	= L.marker([47.229502, -2.175784]).addTo(map);
+
+markergare.bindPopup("Gare SNCF");
+markerbusgare.bindPopup("Arrêt de bus \"Gare SNCF\"");
+markerbusmairie.bindPopup("Arrêt de bus \"Immaculée\"");
+markermairieimmaculee.bindPopup("Mairie");
+markercasinostbrevin.bindPopup("Restaurant").openPopup();
+
+/*
 function initMap() {
 	var labelB = {label: 'B'}
 	var gare 			= {lat: 47.286429, lng: -2.210980};
@@ -32,3 +50,4 @@ function initMap() {
 		map: map
 	});
 }
+*/
